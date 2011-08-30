08f67fb49871e4230eb899dea3881b2e932761e0
Denis Fomin <fominde@gmail.com>
2011-08-30 18:23:56 +0300

    set_location.translation fix

--- set_location/set_location.py
+++ set_location/set_location.py
@@ -73,7 +73,7 @@ class SetLocationPluginConfigDialog(GajimPluginConfigDialog):
         self.GTK_BUILDER_FILE_PATH = self.plugin.local_file_path(
                 'config_dialog.ui')
         self.xml = gtk.Builder()
-        self.xml.set_translation_domain('setlocation')
+        self.xml.set_translation_domain('gajim_plugins')
         self.xml.add_objects_from_file(self.GTK_BUILDER_FILE_PATH,
                 ['hbox1'])
         hbox = self.xml.get_object('hbox1')
