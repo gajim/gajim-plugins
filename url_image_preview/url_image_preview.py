9671c3640cecdc508a7dfd5a4f8d279ddd55f1c6
Denis Fomin <fominde@gmail.com>
2011-11-15 22:59:34 +0300

    url_image_preview.fix var name

--- url_image_preview/url_image_preview.py
+++ url_image_preview/url_image_preview.py
@@ -84,7 +84,7 @@ class Base(object):
         mark = buffer_.create_mark(None, iter_, True)
         # start downloading image
         gajim.thread_interface(helpers.download_image, [
-            self.textview.account, {'src': url}], self._update_img, [mark])
+            self.textview.account, {'src': special_text}], self._update_img, [mark])
 
     def _update_img(self, (mem, alt), mark):
         if mem:
