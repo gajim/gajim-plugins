5d2fef7e058f62b08ede0701fa78500ccd944828
Denis Fomin <fominde@gmail.com>
2012-11-15 12:19:05 +0400

    ClientsIconsPlugin. version 3.0

--- clients_icons/clients_icons.py
+++ clients_icons/clients_icons.py
@@ -162,7 +162,8 @@ class ClientsIconsPlugin(GajimPlugin):
     @log_calls('ClientsIconsPlugin')
     def init(self):
         self.description = _('Shows the client icons in the roster'
-            ' and in groupchats.\nDepends: Gajim > r12506')
+            ' and in groupchats.\nFor icons in tooltip support, '
+            'you need to install Gajim r14117 and above.')
         self.pos_list = [_('after statusicon'), _('before avatar')]
         self.events_handlers = {'presence-received':
                                     (ged.POSTGUI, self.presence_received),
