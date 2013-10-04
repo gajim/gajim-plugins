0c91f1a9c244b419ced2bc6425277d0781c2ec7b
Denis Fomin <fominde@gmail.com>
2013-10-04 22:49:48 +0400

    Whiteboard.Fix for gajim version > 0.15

--- whiteboard/plugin.py
+++ whiteboard/plugin.py
@@ -476,10 +476,7 @@ common.jingle_content.contents[NS_JINGLE_XHTML] = get_content
 
 class JingleTransportSXE(JingleTransport):
     def __init__(self, node=None):
-        if gajim.config.get('version') == '0.15':
-            JingleTransport.__init__(self, TransportType.streaming)
-        else:
-            JingleTransport.__init__(self, TransportType.SOCKS5)
+        JingleTransport.__init__(self, TransportType.streaming)
 
     def make_transport(self, candidates=None):
         transport = JingleTransport.make_transport(self, candidates)
