c9b64a5911e5ff878b7a57a0de4303930a5a3a0b
Bahtiar `kalkin-` Gadimov <bahtiar@gadimov.de>
2016-01-12 12:58:39 +0100

    FIx LOG_DB errors
    We did not do a conversion to unicode as needed

--- omemo/omemo/state.py
+++ omemo/omemo/state.py
@@ -146,7 +146,7 @@ class OmemoState:
                     sid))
                 return
 
-        result = aes_decrypt(key, iv, payload)
+        result = unicode(aes_decrypt(key, iv, payload))
         log.debug("Decrypted msg ⇒ " + result)
         return result
 
