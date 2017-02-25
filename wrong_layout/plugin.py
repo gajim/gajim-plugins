addabc29ae67dbc09d57f8eaaf31e0e3f0510ffe
Philipp Hörist <forenjunkie@chello.at>
2017-02-25 18:20:23 +0100

    [wrong_layout] Fix not escaped html chars

--- wrong_layout/plugin.py
+++ wrong_layout/plugin.py
@@ -13,7 +13,7 @@ class WrongLayoutPlugin(GajimPlugin):
     @log_calls('WrongLayoutPlugin')
     def init(self):
         self.description = _('Press alt+r to convert chars typed in '
-            'wrong layout Rus<>Eng')
+            'wrong layout Rus&lt;&gt;Eng')
         self.config_dialog = None
         self.gui_extension_points = {
                 'chat_control_base': (self.connect_with_chat_control,
