a5460a5d6018106f3a41eb17ffa5c0596d99b8b5
Philipp Hörist <philipp@hoerist.com>
2018-11-02 19:45:12 +0100

    [wicd] Fix pylint errors

--- wicd_support/wicd_support.py
+++ wicd_support/wicd_support.py
@@ -1,17 +1,19 @@
 import os
 
 from gajim.common import app
+from gajim.common import dbus_support
+
 from gajim.plugins import GajimPlugin
 from gajim.plugins.helpers import log_calls
 from gajim.plugins.plugins_i18n import _
-from gajim.common import dbus_support
 
 
 class WicdPlugin(GajimPlugin):
     @log_calls('WicdPlugin')
     def init(self):
-        self.description = _('Support for autodetection of network status '
-        'for Wicd Network Manager.\nRequires wicd and python-dbus.')
+        self.description = _(
+            'Support for autodetection of network status '
+            'for Wicd Network Manager.\nRequires wicd and python-dbus.')
         self.config_dialog = None
         self.test_activatable()
 
@@ -24,7 +26,7 @@ class WicdPlugin(GajimPlugin):
         if not dbus_support.supported:
             self.activatable = False
             self.available_text += _('python-dbus is missing! '
-                'Install python-dbus.')
+                                     'Install python-dbus.')
 
     @log_calls('WicdPlugin')
     def activate(self):
@@ -37,27 +39,33 @@ class WicdPlugin(GajimPlugin):
             if 'org.wicd.daemon' not in self.bus.list_names():
                 return
             wicd_object = self.bus.get_object('org.wicd.daemon',
-                    '/org/wicd/daemon')
+                                              '/org/wicd/daemon')
             self.props = dbus.Interface(wicd_object,
-                "org.freedesktop.DBus.Properties")
-            self.bus.add_signal_receiver(self.state_changed, 'StatusChanged',
-                'org.wicd.daemon', 'org.wicd.daemon', '/org/wicd/daemon')
+                                        'org.freedesktop.DBus.Properties')
+            self.bus.add_signal_receiver(self.state_changed,
+                                         'StatusChanged',
+                                         'org.wicd.daemon',
+                                         'org.wicd.daemon',
+                                         '/org/wicd/daemon')
         except dbus.DBusException:
             pass
 
     @log_calls('WicdPlugin')
     def deactivate(self):
-        self.bus.remove_signal_receiver(self.state_changed, 'StatusChanged',
-            'org.wicd.daemon', 'org.wicd.daemon','/org/wicd/daemon')
+        self.bus.remove_signal_receiver(self.state_changed,
+                                        'StatusChanged',
+                                        'org.wicd.daemon',
+                                        'org.wicd.daemon',
+                                        '/org/wicd/daemon')
 
-    def state_changed(self, state, info):
+    def state_changed(self, state, _info):
         # Connection state constants
-        #NOT_CONNECTED = 0
-        #CONNECTING = 1
-        #WIRELESS = 2
-        #WIRED = 3
-        #SUSPENDED = 4
-        if state == 2 or state == 3:
+        # NOT_CONNECTED = 0
+        # CONNECTING = 1
+        # WIRELESS = 2
+        # WIRED = 3
+        # SUSPENDED = 4
+        if state in (2, 3):
             for connection in app.connections.values():
                 if app.config.get_per('accounts', connection.name,
                 'listen_to_network_manager') and connection.time_to_reconnect:
