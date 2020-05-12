acba4b6993a0de630d504f165d0596d7b726c349
Daniel Brötzmann <mailtrash@posteo.de>
2020-05-12 11:33:54 +0200

    [plugins_translations] Update translations

--- plugins_translations/plugins_translations.py
+++ plugins_translations/plugins_translations.py
@@ -1,26 +1,24 @@
-# -*- coding: utf-8 -*-
-##
-
+import logging
 import os
 
 from gajim.common import configpaths
+
 from gajim.plugins import GajimPlugin
-from gajim.plugins.helpers import log_calls
 from gajim.plugins.plugins_i18n import _
+
+log = logging.getLogger('gajim.p.plugins_translations')
 
 
 class PluginsTranslationsPlugin(GajimPlugin):
-
-    @log_calls('PluginsTranslationsPlugin')
     def init(self):
-        self.description = _('This plugin contains translation files '
-                             'for Gajim plugins')
+        self.description = _('This plugin contains translations for other '
+                             'Gajim plugins. Please restart Gajim after '
+                             'enabling this plugin.')
         self.config_dialog = None
         self.config_default_values = {'last_version': '0'}
         self.locale_dir = os.path.join(
             configpaths.get('PLUGINS_USER'), 'locale')
 
-    @log_calls('PluginsTranslationsPlugin')
     def activate(self):
         if self.config['last_version'] == self.version:
             return
@@ -30,25 +28,28 @@ class PluginsTranslationsPlugin(GajimPlugin):
         files = glob(self.__path__ + '/*.mo')
 
         # remove old data
-        self.remove_translations()
+        self._remove_translations()
 
         # create dirs and copy files
         os.mkdir(self.locale_dir)
-        locales = [os.path.splitext(os.path.basename(name))[0] for name in files]
+        locales = [
+            os.path.splitext(os.path.basename(name))[0] for name in files
+        ]
+        log.info('Installing new translations...')
         for locale in locales:
             dst = os.path.join(os.path.join(self.locale_dir, locale),
-                'LC_MESSAGES/gajim_plugins.mo')
+                               'LC_MESSAGES/gajim_plugins.mo')
             os.makedirs(os.path.split(dst)[0])
             shutil.copy2(os.path.join(self.__path__, '%s.mo' % locale), dst)
 
         self.config['last_version'] = self.version
 
-    def remove_translations(self):
+    def _remove_translations(self):
+        log.info('Removing old translations...')
         if os.path.isdir(self.locale_dir):
             import shutil
             shutil.rmtree(self.locale_dir)
 
-    @log_calls('PluginsTranslationsPlugin')
     def deactivate(self):
-        self.remove_translations()
+        self._remove_translations()
         self.config['last_version'] = '0'
