3db0e1fea21d8b4f8ec8d697138036fa9309d5a8
lovetox <philipp@hoerist.com>
2022-04-29 18:59:28 +0200

    [plugin_installer] Remove plugin
    This is integrated into Gajim now

--- plugin_installer/remote.py
+++
@@ -1,8 +0,0 @@
-# File which defines all remote URLs
-
-server = 'https://ftp.gajim.org'
-directory = 'plugins_master_zip'
-
-PLUGINS_DIR_URL = '%s/%s' % (server, directory)
-MANIFEST_URL = '%s/manifests.zip' % PLUGINS_DIR_URL
-MANIFEST_IMAGE_URL = '%s/manifests_images.zip' % PLUGINS_DIR_URL
