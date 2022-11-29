017e7a753fce714164551e176645dcddb55c7d2b
wurstsalat <mailtrash@posteo.de>
2022-11-29 19:33:23 +0100

    [message_box_size] Type annotations, linting

--- message_box_size/config_dialog.py
+++ message_box_size/config_dialog.py
@@ -1,3 +1,24 @@
+#
+# This file is part of Gajim.
+#
+# Gajim is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Gajim is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Gajim. If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import annotations
+
+from typing import Any
+from typing import TYPE_CHECKING
+
 from gi.repository import GObject
 from gi.repository import Gtk
 
@@ -8,25 +29,35 @@ from gajim.gui.const import SettingType
 
 from gajim.plugins.plugins_i18n import _
 
+if TYPE_CHECKING:
+    from .msg_box_size import MsgBoxSizePlugin
+
 
 class MessageBoxSizeConfigDialog(SettingsDialog):
-    def __init__(self, plugin, parent):
+    def __init__(self, plugin: MsgBoxSizePlugin, parent: Gtk.Window) -> None:
 
         self.plugin = plugin
         settings = [
-            Setting('PreviewSizeSpinSetting', _('Height in pixels'),
-                    SettingType.VALUE, self.plugin.config['HEIGHT'],
-                    callback=self.on_setting, data='HEIGHT',
+            Setting('PreviewSizeSpinSetting',  # type: ignore
+                    _('Height in pixels'),
+                    SettingType.VALUE,
+                    self.plugin.config['HEIGHT'],
+                    callback=self._on_setting,
+                    data='HEIGHT',
                     desc=_('Size of message input in pixels'),
                     props={'range_': (20, 200)}),
             ]
 
-        SettingsDialog.__init__(self, parent, _('Message Box Size Configuration'),
-                                Gtk.DialogFlags.MODAL, settings, None,
-                                extend=[('PreviewSizeSpinSetting',
+        SettingsDialog.__init__(self,
+                                parent,
+                                _('Message Box Size Configuration'),
+                                Gtk.DialogFlags.MODAL,
+                                settings,
+                                '',
+                                extend=[('PreviewSizeSpinSetting',  # type: ignore
                                          SizeSpinSetting)])
 
-    def on_setting(self, value, data):
+    def _on_setting(self, value: Any, data: Any) -> None:
         self.plugin.config[data] = value
         self.plugin.set_input_height(value)
 
@@ -34,8 +65,14 @@ class MessageBoxSizeConfigDialog(SettingsDialog):
 class SizeSpinSetting(SpinSetting):
 
     __gproperties__ = {
-        "setting-value": (int, 'Size', '', 20, 200, 20,
-                          GObject.ParamFlags.READWRITE), }
+        'setting-value': (int,
+                          'Size',
+                          '',
+                          20,
+                          200,
+                          20,
+                          GObject.ParamFlags.READWRITE),
+    }
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
         SpinSetting.__init__(self, *args, **kwargs)
